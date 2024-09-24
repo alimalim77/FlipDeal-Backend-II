e4aa34f933a9645197b3b7d776b6421370e1842b
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2024-09-24 22:53:52 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const port = 3010;
+
+app.get('/calculate-returns', (req, res) => {
+  const { boughtAt, marketPrice, quantity } = req.query;
+  const calculateReturn = (boughtAt, marketPrice, quantity) =>
+    (marketPrice - boughtAt) * 2;
+  res.json(
+    calculateReturn(
+      parseFloat(boughtAt),
+      parseFloat(marketPrice),
+      parseFloat(quantity)
+    ).toString()
+  );
+});
+
+app.get('/total-returns', (req, res) => {
+  const { stock1, stock2, stock3, stock4 } = req.query;
+  const totalReturn = (...args) => args.reduce((acc, sum) => acc + sum, 0);
+  res.json(
+    totalReturn(
+      parseFloat(stock1),
+      parseFloat(stock2),
+      parseFloat(stock3),
+      parseFloat(stock4)
+    ).toString()
+  );
+});
+
+app.get('/calculate-return-percentage', (req, res) => {
+  const { boughtAt, returns } = req.query;
+  const totalReturn = (boughtAt, returns) => (returns / boughtAt) * 100;
+  res.json(totalReturn(parseFloat(boughtAt), parseFloat(returns)).toString());
+});
+
+app.get('/total-return-percentage', (req, res) => {
+  const { stock1, stock2, stock3, stock4 } = req.query;
+  const totalReturn = (...args) => args.reduce((acc, sum) => acc + sum, 0);
+  res.json(
+    totalReturn(
+      parseFloat(stock1),
+      parseFloat(stock2),
+      parseFloat(stock3),
+      parseFloat(stock4)
+    ).toString()
+  );
+});
+
+app.get('/status', (req, res) => {
+  const { returnPercentage } = req.query;
+  res.json(parseFloat(returnPercentage) > 0 ? 'profit' : 'loss');
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
